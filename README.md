009276caabfff8ec1ef4cc1958e1fcb77bd8651b
Kryštof Matějka <kx.matejka@gmail.com>
2021-01-05 20:09:02 +0100

    add cost function

--- README.md
+++ README.md
@@ -15,3 +15,11 @@ Simple template for js web application
 npm install
 npm run dev
 ```
+
+- https://www.youtube.com/watch?v=L-Lsfu4ab74
+- https://www.youtube.com/watch?v=_cXuvTQl090
+- https://www.youtube.com/watch?v=szXbuO3bVRk
+- https://www.youtube.com/watch?v=jc2IthslyzM
+
+- https://www.robinwieruch.de/linear-regression-gradient-descent-javascript
+- https://github.com/javascript-machine-learning/linear-regression-gradient-descent
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,31 +1,86 @@
 import React, {useState, FC} from 'react'
-import {Stage, Layer, Rect, Circle} from 'react-konva'
-
-const CANVAS_WIDTH = 500
-const CANVAS_HEIGHT = 500
+import {Stage, Layer, Rect, Circle, Line} from 'react-konva'
 
 interface Point {
   x: number,
   y: number,
 }
 
+const CANVAS_WIDTH = 500
+const CANVAS_HEIGHT = 500
+
+const h = (x: number, m: number, b: number) => m * x + b
+
+const cost = (points: Point[], m: number, b: number) => {
+  const M = points.length
+  if (!M) {
+    return 0
+  }
+
+  let sum = 0
+  for (const point of points) {
+    sum += (h(point.x, m, b) - point.y) ** 2
+  }
+
+  return sum / (2 * M)
+}
+
 const Point: FC<{point: Point}> = ({point}) => {
   return <Circle radius={5} x={point.x} y={point.y} fill={'#2d9cf1'}/>
 }
 
+const FunctionLine: FC<{m: number, b: number}> = ({m, b}) => {
+  const x1 = 0
+  const y1 = h(0, m, b)
+
+  const x2 = CANVAS_WIDTH
+  const y2 = h(CANVAS_WIDTH, m, b)
+
+  return <Line points={[x1, y1, x2, y2]} stroke={'#e33939'} width={10}/>
+}
+
 export const Home = () => {
   const [points, setPoints] = useState<Point[]>([])
+  const [m, setM] = useState(0)
+  const [b, setB] = useState(0)
+  const totalCost = cost(points, m, b)
 
   return (
-    <Stage width={CANVAS_WIDTH} height={CANVAS_HEIGHT}>
-      <Layer>
-        <Rect
-          width={CANVAS_WIDTH} height={CANVAS_HEIGHT} fill={'#dbdbdb'}
-          onClick={(e) => setPoints((points) => [...points, {x: e.evt.x, y: e.evt.y}])}/>
-      </Layer>
-      <Layer>
-        {points.map((point, index) => <Point key={index} point={point}/>)}
-      </Layer>
-    </Stage>
+    <>
+      <Stage width={CANVAS_WIDTH} height={CANVAS_HEIGHT}>
+        <Layer>
+          <Rect
+            width={CANVAS_WIDTH} height={CANVAS_HEIGHT} fill={'#ececec'}
+            onClick={(e) => setPoints((points) => [...points, {x: e.evt.x, y: e.evt.y}])}/>
+        </Layer>
+        <Layer>
+          {points.map((point, index) => <Point key={index} point={point}/>)}
+        </Layer>
+        <Layer>
+          <FunctionLine m={m} b={b}/>
+        </Layer>
+      </Stage>
+      <div>
+        cost:&nbsp;{totalCost}
+      </div>
+      <div>
+        <label>
+          m:&nbsp;
+          <input
+            type={'number'} value={m} step={0.1}
+            onChange={(event) => setM(Number(event.target.value))}
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          b:&nbsp;
+          <input
+            type={'number'} value={b}
+            onChange={(event) => setB(Number(event.target.value))}
+          />
+        </label>
+      </div>
+    </>
   )
 }
